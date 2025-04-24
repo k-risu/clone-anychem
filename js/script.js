cae0d21e1e1ea62822347d6c3a3408892bda637b
k-risu <kkd4869@gmail.com>
2025-04-24 11:51:06 +0900

    [feat] 애니켐 클론코딩

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function () {
+  // 모달창 기능
+  let modalWrap = document.querySelector(".modal-wrap");
+  let modalClose = document.querySelector(".modal-close");
+  modalClose.addEventListener("click", function () {
+    modalWrap.classList.add("fadeOut");
+    modalWrap.addEventListener("animationend", () => {
+      modalWrap.style.display = "none";
+    });
+  });
+
+  AOS.init();
+});
